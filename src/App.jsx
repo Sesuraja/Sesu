13d4d551b208c8ff47fd787dcf47923c93ab5cda
SESURAJA <sesuraja771@gmail.com>
2024-10-28 10:05:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
+import { HashRouter } from 'react-router-dom';
 import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
+      <HashRouter>
       <Router>
         <Navbar />
         <Routes>
@@ -25,6 +26,7 @@ const App = () => {
           />
         </Routes>
       </Router>
+        </HashRouter>
     </main>
   );
 };
